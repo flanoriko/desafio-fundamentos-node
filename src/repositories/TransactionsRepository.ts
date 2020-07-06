1678ec81dbe6193c34b9b69f0cc4d2a2e4b451a1
Flavia Fukuda <flavia.noriko.work@gmail.com>
2020-07-05 23:10:34 -0300

    POST /transactions: A rota deve receber title, value e type dentro do corpo da requisição, sendo type o tipo da transação, que deve ser income para entradas (depósitos) e outcome para saídas (retiradas). Ao cadastrar uma nova transação, ela deve ser armazenada dentro de um objeto com o seguinte formato : {   "id": "uuid",   "title": "Salário",   "value": 3000,   "type": "income" } GET /transactions: Essa rota deve retornar uma listagem com todas as transações que você cadastrou até agora, junto com o valor de soma de entradas, retiradas e total de crédito. Essa rota deve retornar um objeto com o formato a seguir: {   "transactions": [     {       "id": "uuid",       "title": "Salário",       "value": 4000,       "type": "income"     },     {       "id": "uuid",       "title": "Freela",       "value": 2000,       "type": "income"     },     {       "id": "uuid",       "title": "Pagamento da fatura",       "value": 4000,       "type": "outcome"     },     {       "id": "uuid",       "title": "Cadeira Gamer",       "value": 1200,       "type": "outcome"     }   ],   "balance": {     "income": 6000,     "outcome": 5200,     "total": 800   } } Dica: Dentro de balance, o income é a soma de todos os valores das transações com type income. O outcome é a soma de todos os valores das transações com type outcome, e o total é o valor de income - outcome.
    Dica 2: Para fazer a soma dos valores, você pode usar a função reduce para agrupar as transações pela propriedade type, assim você irá conseguir somar todos os valores com facilidade e obter o retorno do balance.
    
    Especificação dos testes
    Em cada teste, tem uma breve descrição no que sua aplicação deve cumprir para que o teste passe.
    
    Caso você tenha dúvidas quanto ao que são os testes, e como interpretá-los, dé uma olhada em nosso FAQ.
    
    Para esse desafio temos os seguintes testes:
    
    should be able to create a new transaction: Para que esse teste passe, sua aplicação deve permitir que uma transação seja criada, e retorne um json com a transação criada.
    
    should be able to list the transactions: Para que esse teste passe, sua aplicação deve permitir que seja retornado um objeto contendo todas as transações junto ao balanço de income, outcome e total das transações que foram criadas até o momento.
    
    should not be able to create outcome transaction without a valid balance: Para que esse teste passe, sua aplicação não deve permitir que uma transação do tipo outcome extrapole o valor total que o usuário tem em caixa, retornando uma resposta com código HTTP 400 e uma mensagem de erro no seguinte formato: { error: string }

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,20 +9,50 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    var totalIncome = 0;
+    var totalOutcome = 0;
+    var total = 0;
+
+    function somar(value: number, type: 'income' | 'outcome') {
+      if (type === "income") {
+        totalIncome = totalIncome + value;
+      }
+      else {
+        if (type === "outcome") {
+          totalOutcome = totalOutcome + value;
+        }
+      }
+      total = totalIncome - totalOutcome;
+    }
+    this.transactions.forEach(trans => somar(trans.value, trans.type));
+    const balance: Balance = { income: totalIncome, outcome: totalOutcome, total: total };
+
+    return balance;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create(title: string, value: number, type: 'income' | 'outcome'): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    const balance =  this.getBalance();
+
+    if (type ==="outcome" && balance.total - value < 0)
+    {
+      throw Error ('Valor extrapola o saldo');
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,30 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+
+interface Response {
+  transactions: Transaction;
+  balance: {
+    income: number;
+    outcome: number;
+    total: number;
+  }
+}
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    const concatResult = { transactions, balance };
+
+
+    return response.json(concatResult);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +32,11 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    const createTransactionService = new CreateTransactionService(transactionsRepository);
+    const newTransaction = createTransactionService.execute({ title, value, type });
+    return response.json(newTransaction);
+
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
